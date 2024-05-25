03982fb26f7cef89d7c884e71986f85fbb9f4aab
sandrasong <chromsketch@gmail.com>
2024-05-25 17:26:43 -0400

    Add balance display.

--- app/components/Account.tsx
+++ app/components/Account.tsx
@@ -1,18 +1,33 @@
 "use client"
 
-import { useAccount, useDisconnect, useEnsAvatar, useEnsName } from "wagmi"
+import { useAccount, useDisconnect, useEnsAvatar, useEnsName, useBalance, useChainId } from "wagmi"
 
 export function Account() {
   const { address } = useAccount()
   const { disconnect } = useDisconnect()
+  const chainId = useChainId()
   const { data: ensName } = useEnsName({ address })
   const { data: ensAvatar } = useEnsAvatar({ name: ensName! })
+  const { data, isError, isLoading } = useBalance({
+    address: address,
+    chainId: chainId,
+  })
 
   return (
     <div>
       {ensAvatar && <img alt="ENS Avatar" src={ensAvatar} />}
       {address && <div>{ensName ? `${ensName} (${address})` : address}</div>}
-      <button onClick={() => disconnect()}>Disconnect</button>
+      {isError && <div>Error fetching balance</div>}
+      <div>
+        {" "}
+        Balance: {data?.formatted} {data?.symbol}{" "}
+      </div>
+      <button
+        onClick={() => disconnect()}
+        className="mt-2 rounded-lg border border-gray-400 px-4 py-3 transition-colors bg-gradient-to-b from-zinc-200"
+      >
+        Disconnect
+      </button>
     </div>
   )
 }
--- config.ts
+++ config.ts
@@ -1,10 +1,10 @@
 import { http, createConfig, cookieStorage, createStorage } from "wagmi"
 import { localhost, sepolia, mainnet } from "wagmi/chains"
-import { injected, metaMask, safe } from "wagmi/connectors"
+import { injected, coinbaseWallet } from "wagmi/connectors"
 
 export const config = createConfig({
   chains: [mainnet, localhost, sepolia],
-  connectors: [injected(), metaMask(), safe()],
+  connectors: [injected(), coinbaseWallet()],
   transports: {
     [mainnet.id]: http(),
     [localhost.id]: http(),
